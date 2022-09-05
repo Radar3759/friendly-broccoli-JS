7cb11925ca079b80255dc498a2c39d8034ddfae5
Radar3759 <bunnybridgemitten@gmail.com>
2022-09-05 11:54:49 -0700

    -rename js to app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// comment your JavaScript (JS) Code
+/* multi-line comment
+Yes, multi-pass
+*/
+//single line comment
+
+// Declare JS Variable
+var myName;
+
+// Storing Values with the Assignment Operator
+var a;
+a = 7;
+console.log(a);
+
+
+
