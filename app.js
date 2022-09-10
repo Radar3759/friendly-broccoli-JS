28dad6896d9e8bf00072d2dd8a5238a341c79e12
Radar3759 <bunnybridgemitten@gmail.com>
2022-09-10 11:50:43 -0700

    -through Into If Else Stmts

--- app.js
+++ app.js
@@ -519,4 +519,63 @@ function testLessOrEqual(val) {
 
 testLessOrEqual(10);
 
-// 
+// Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+// Comparisons with the Logical Or Operator
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+// Introducing Else Statements
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+// Introducing If Else Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+  return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
+
+// Logical Order in If Else Statements
