2d1901e534e5450b14ede19b77a3ae334b1a8d54
Radar3759 <bunnybridgemitten@gmail.com>
2022-09-14 18:25:13 -0700

    -through Gen Ran Num c JS

--- app.js
+++ app.js
@@ -24,16 +24,16 @@ b = a;
 var a = 9;
 
 // Declar String Variables
-var myFirstName= "First";
+var myFirstName = "First";
 var myLastName = "Last";
 console.log(myFirstName);
 console.log(Last);
 
 // Understanding Uninitialized Variables
 // Only change code below this line
-var a= 5;
-var b= 10;
-var c= "I am a";
+var a = 5;
+var b = 10;
+var c = "I am a";
 // Only change code above this line
 
 a = a + 1;
@@ -87,7 +87,7 @@ var myDecimal = 5.7;
 // Multiply Two Decimals with JavaScript
 const product = 2.0 * 2.5;
 // Divide One Decimal by Another with JavaScript
-const quotient = 4.4/2.0; // Change this line
+const quotient = 4.4 / 2.0; // Change this line
 // Finding a Remainder in JavaScript
 const remainder = 11 % 3;
 // Compound Assignment With Augmented Addition
@@ -139,8 +139,8 @@ const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 const myStr = "This is the start. " + "This is the end."; // Change this line
 
 //Concatenating Strings with the Plus Equals Operator
-let myStr= "This is the first sentence. ";
-myStr += "This is the second sentence." ;
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
 
 // Constructing STrings with Variables
 // Only change code below this line
@@ -178,7 +178,7 @@ console.log(firstLetterOfLastName);
 let myStr = "Jello World";
 
 // Only change code below this line
-myStr= "Hello World"; // Change this line
+myStr = "Hello World"; // Change this line
 // Only change code above this line
 
 // Use Bracket Notation
@@ -193,14 +193,14 @@ const thirdLetterOfLastName = lastName[2]; // Change this line
 const lastName = "Lovelace";
 
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length -1]; // Change this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
 
 // Use Bracket Notation to Find the Nth-to-Last Character in a String
 // Setup
 const lastName = "Lovelace";
 
 // Only change code below this line
-const secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
 
 // Word Blanks
 const myNoun = "dog";
@@ -209,7 +209,7 @@ const myVerb = "ran";
 const myAdverb = "quickly";
 
 // Only change code below this line
-const wordBlanks = "The " + myAdjective + ", brown " + myNoun + " walked, then " + myVerb + " very " + myAdverb + " toward the park." ; 
+const wordBlanks = "The " + myAdjective + ", brown " + myNoun + " walked, then " + myVerb + " very " + myAdverb + " toward the park.";
 console.log(wordBlanks);
 // Change this line
 // Only change code above this line
@@ -238,14 +238,14 @@ console.log(myArray);
 
 // Access Multi-Dimensional Arrays with Indexes
 const myArray = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14],
-  ];
-  
-  const myData = myArray[2][1];
-  console.log(myData);
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
+console.log(myData);
 
 //   Manipulate Arrays With push()
 // Setup
@@ -280,12 +280,12 @@ myArray.unshift(["Paul", 35]);
 console.log(myArray)
 
 // Shopping List
-const myList = [["Sandwiches", 3],["Fries", 23],["Musubi", 6],["Toast", 28],["Bees", 3]];
+const myList = [["Sandwiches", 3], ["Fries", 23], ["Musubi", 6], ["Toast", 28], ["Bees", 3]];
 console.log(myList);
 
 //Write Reusable JavaScript with Functions
 function reusableFunction() {
-    console.log("Hi World");
+  console.log("Hi World");
 }
 reusableFunction();
 
@@ -293,9 +293,9 @@ reusableFunction();
 function functionWithArgs(var1, var2) {
   console.log(var1 + var2);
 }
-functionWithArgs(2,3);
-functionWithArgs(1,2);
-functionWithArgs(7,9);
+functionWithArgs(2, 3);
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
 
 // Return a Value from a Function with Return
 function timesFive(num) {
@@ -440,7 +440,7 @@ compareEquality(10, "10");
 // Comparison with the Inequality Operator
 // Setup
 function testNotEqual(val) {
-  if (val!=99) { // Change this line
+  if (val != 99) { // Change this line
     return "Not Equal";
   }
   return "Equal";
@@ -451,7 +451,7 @@ testNotEqual(10);
 // Comparison with the Strict Inequality Operator
 // Setup
 function testStrictNotEqual(val) {
-  if (val!==17) { // Change this line
+  if (val !== 17) { // Change this line
     return "Not Equal";
   }
   return "Equal";
@@ -465,7 +465,7 @@ function testGreaterThan(val) {
     return "Over 100";
   }
 
-  if (val > 10 ) {  // Change this line
+  if (val > 10) {  // Change this line
     return "Over 10";
   }
 
@@ -525,7 +525,7 @@ function testLogicalAnd(val) {
 
   if (val <= 50 && val >= 25) {
 
-      return "Yes";
+    return "Yes";
   }
 
   // Only change code above this line
@@ -572,10 +572,630 @@ function testElseIf(val) {
   } else if (val < 5) {
     return "Smaller than 5";
   } else {
-  return "Between 5 and 10";
+    return "Between 5 and 10";
   }
 }
 
 testElseIf(7);
 
 // Logical Order in If Else Statements
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+// Chaining If Else Statements
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
+  } else {
+    return "Change Me";
+  }
+  // Only change code above this line
+}
+
+testSize(7);
+
+// Golf Code
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes === 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes === par) {
+    return "Par";
+  } else if (strokes === par + 1) {
+    return "Bogey";
+  } else if (strokes === par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  } else {
+    return "Change Me";
+  }
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+// Selecting from many options with Switch Statements
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+caseInSwitch(1);
+
+// Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    default:
+      return "stuff";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+// function sequentialSizes(val) {
+let answer = "";
+// Only change code below this line
+switch (val) {
+  case 1:
+  case 2:
+  case 3:
+    answer = "Low";
+    break;
+  case 4:
+  case 5:
+  case 6:
+    answer = "Mid";
+    break;
+  case 7:
+  case 8:
+  case 9:
+    answer = "High";
+    break;
+}
+
+
+// Only change code above this line
+return answer;
+}
+
+sequentialSizes(1);
+
+// Replacing If/Else with Chain
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+// Returning Boolean Values from Functions
+function isLess(a, b) {
+  // Only change code below this line
+  return a <= b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+// Return Early Pattern for Function
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0)
+    return undefined;
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
+// Counting Cards
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// Build JavaScript Objects
+const myDog = {
+  // Only change code below this line
+  "name": "Fido",
+  "legs": 3,
+  "tails": 1,
+  "friends": ["Frodo", "Spot"]
+
+  // Only change code above this line
+};
+
+// Accessing Object Properties with Dot Notation
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+// Accessing Object Properties with Bracket Notation
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+
+// Accessing Object Properties with Variables
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+// Updating Object Properties
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog["name"] = "Happy Coder";
+
+// Add New Properties to a JavaScript Object
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "RAWA!"
+
+// Delete Properties from a Javascript Object
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
+
+// Using Objects for Lookups
+const lookup = {
+  "alpha": "Adams",
+  "bravo":"Boston",
+  "charlie": "Chicago",
+  "delta": "Denver",
+  "echo": "Easy",
+  "foxtrot": "Frank",
+}
+// Setup
+function phoneticLookup(val) {
+  let result = lookup[val];
+
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+
+// Testing Objects for Properties
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  }
+  return "Change Me!";
+  // Only change code above this line
+}
+
+const myBird= {
+  "canFly": true,
+  "color": "Red",
+}
+
+console.log(checkObj(myBird, "color"));
+
+// Manipulating Complex Objects
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }, 
+];
+
+const newAlbum = {
+    "artist": "Jimmies Chicken Shack",
+    "title": "Bring Your Own Stereo",
+    "release_year": 1999,
+    "formats": [
+      "CD",
+      "LP"
+    ],  
+    "gold": true
+}
+
+myMusic.push(newAlbum);
+console.log(myMusic);
+
+//Accessing Nested Objects
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+console.log(gloveBoxContents);
+
+// Accessing Nested Arrays
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+console.log(secondTree);
+
+// Record Collection
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+
+function updateRecords(records, id, prop, value) {
+  console.log(records[id]);
+  if (prop != "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && !records[id].hasOwnProperty("tracks")) {
+    records[id].tracks = [];
+    records[id].tracks.push(value);
+  } else if (prop === "tracks" && value!== "") {
+    records[id].tracks.push(value);
+  } else  if (value === "") {
+    delete records[id][prop];
+  }
+
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+// Iterate with JS While Loops
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 0;
+while (i < 6) {
+  myArray.unshift(i);
+  i++;
+}
+console.log(myArray);
+
+//Iterate with JavaScript For Loops
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 6; i++) {
+  myArray.push(i);
+}
+console.log(myArray);
+
+// Iterate Odd Nums
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+console.log(myArray);
+
+// Count Backwards With a For Loop
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i -=2) {
+  myArray.push(i)
+}
+console.log(myArray);
+
+// Iterate Through an Array with a For Loop
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+  total = myArr[i] + total;
+}
+
+console.log(total);
+
+// Nesting For Loops
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product = product * (arr[i][j]);
+    }
+  }
+      console.log(product);
+  // Only change code above this line
+  return product;
+  
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+//Iterate with JavaScript Do...While Loops
+// Setup
+const myArray = [];
+let i = 10;
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 11);
+
+console.log(i);
+
+//Replace Loops Using Recursion
+function sum(arr, n) {
+  // Only change code below this line
+  // base to stop infinite loop
+  if (n <= 0) {
+    return 0;
+  // recusive call
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+  // Only change code above this line
+}
+
+// Profile Lookup
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (var i = 0; i < contacts.length; i++) {
+    if(contacts[i].firstName === name) {
+      // ok to use || here instead of another line
+      return contacts[i][prop] || "No such property";
+      } 
+    }
+    return "No such contact"; 
+  }
+  // Only change code above this line
+
+
+// var data = lookUpProfile("Akira", "likes");
+var data = lookUpProfile("Sherlock", "likes");
+console.log(data);
+
+// Generate Random Fractions with JS
+function randomFraction() {
+
+  // Only change code below this line
+
+  return (Math.random());
+
+  // Only change code above this line
+} 
+
+// Generate Random Whole Numbers with Javascript
+function randomWholeNum() {
+
+  // Only change code below this line
+
+  return Math.floor(Math.random() * 10);
+}
+
+// Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return (Math.floor(Math.random() * (myMax - myMin + 1)) + myMin);
+  // Only change code above this line
+}
+
+// Use the parseInt Function
+function convertToInteger(str) {
+  var b = parseInt(str);
+   return b;
+ }
+ 
+ convertToInteger("56");
